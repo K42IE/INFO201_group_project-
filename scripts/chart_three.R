2d663fd76487fca5dc4380380094237d673419d7
Katherine Pryal <katiepryal@live.com>
2020-11-30 14:30:44 -0800

    added third chart for Sabrina

---
+++ scripts/chart_three.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+data <- read.csv("data/final_data.csv", stringsAsFactors = FALSE)
+
+make_chart_three <- function(data) {
+  top_highest <- data %>%
+    arrange(desc(co2.per.capita)) %>%
+    tail()
+  
+ chart <- ggplot(top_highest, aes(x = "", y = Healthy.life.expectancy, fill = Country.or.region)) +
+    geom_bar(stat = "identity", with = 1) +
+    coord_polar("y", start = 0) +
+   ggtitle("Healthy Life Expectanncy for Countries with the Lowest CO2 Emissions") +
+   labs(y = "", x = "")
+  
+  return(chart)
+}
+
