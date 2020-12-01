9254db0c8ce9686c96926b3677d64d5737e24426
Sydney Burns <Sydney@Sydneys-MacBook-Air.local>
2020-11-30 17:05:30 -0800

    updated charts

---
+++ scripts/bar_chart.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(dplyr)
+library(plotly)
+
+# This document creates a bar chart for our data frame for the report
+
+co2_happiness_bar <- function(df) {
+  lowest_co2 <- df %>%
+  top_n(5, wt = -co2.per.capita)
+  lowest_co2_plot <- ggplot(data = lowest_co2) +
+    geom_bar(mapping = aes(x = Country.or.region, weight = Score)) +
+    labs(y = "Happiness Score", x = "Country") +
+    ggtitle("Happiness Scores for the Lowest CO2 Per Capita Countries")
+  lowest_co2_plot_interactive <- ggplotly(lowest_co2_plot, tooltip = "Score")
+  return(lowest_co2_plot_interactive)
+}
