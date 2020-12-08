b7830ab5342025413775f2898d00d3c13243d91f
Sydney Burns <Sydney@Sydneys-MacBook-Air.local>
2020-12-07 19:54:44 -0800

    updates

--- app_server.R
+++ app_server.R
@@ -1,3 +1,5 @@
+#server
+
 library(plotly)
 library(ggplot2)
 
@@ -14,12 +16,17 @@ plot_final_data <- final_data %>%
 server <- function(input, output) {
   output$scatter <- renderPlot({
     
-    title <- paste0("Scatter Plot:", input$x_var, " v. ", input$y_var)
+    title <- paste0("Scatter Plot: ", input$x_var, " v. ", input$y_var)
     
     
     plot <- ggplot(data = plot_final_data) +
       geom_point(mapping = aes_string(x = input$x_var, y = input$y_var)) +
       labs(x = input$x_var, y = input$y_var, title = title)
+    
+    if (input$smooth) {
+      plot <- plot + geom_smooth(mapping = 
+                                   aes_string(x = input$x_var, y = input$y_var))
+    }
     
     plot
     
--- app_ui.R
+++ app_ui.R
@@ -8,20 +8,18 @@ gdp_range <- range(final_data$GDP.per.capita)
 
 select_values <- colnames(plot_final_data)
 
+
 x_input <- selectInput(
   "x_var",
   label = "X Variable",
   choices = select_values,
-  selected = "GDP.per.capita"
-)
+  selected = "GDP.per.capita")
 
 y_input <- selectInput(
   "y_var",
   label = "Y Variable",
   choices = select_values,
-  selected = "Social.support"
-)
-
+  selected = "Social.support")
 
 
 
@@ -36,17 +34,30 @@ plot_sidebar_content <- sidebarPanel(
     label = "Y Variable",
     choices = select_values,
     selected = "Social.support"),
+    checkboxInput("smooth", label = strong("Show Trendline"),
+                  value = TRUE)
 )
 
 
 plot_main_content <- mainPanel(
-  plotOutput("scatter")
+  plotOutput("scatter"),
+  p(strong("Visualization Justification:"), "This chart seeks to answer
+    how the variables that make up the overall happiness score
+    are related to each other. Thus, do some variables are 
+    a stronger realtionship than others? Are there any
+    varaibles that have a negative relationship?"),
+    p(strong("Why This Chart:"), "A scatter plot was appropriate in this
+    situation because it clearly allows the users to see
+    the relationship of the different variables. The ability
+    to add a trend line to this plot also furhter clarifies 
+    and highlights the relationship between the selected
+    variables.")
 )
 
 
 plot_panel <- tabPanel(
   "Scatter Plot",
-  titlePanel("Scatter"),
+  titlePanel("Select Scatter Plot Variables"),
   sidebarLayout(
     plot_sidebar_content,
     plot_main_content
