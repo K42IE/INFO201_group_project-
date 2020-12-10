39b8a9f02c2b2d6e90bf23b14177c7165bbd1c30
Sydney Burns <Sydney@Sydneys-MacBook-Air.local>
2020-12-09 20:11:04 -0800

    edits

--- app_server.R
+++ app_server.R
@@ -6,6 +6,7 @@ library(dplyr)
 
 source("./buildScatter.R")
 
+#read in data frame
 final_data <- read.csv("./data/final_data2.csv", stringsAsFactors = FALSE)
 
 server <- function(input, output) {
@@ -25,8 +26,6 @@ server <- function(input, output) {
   
   
   # rest
-  
-  
   
   world_spdf <- readOGR( 
     dsn="world_shape_file", 
@@ -65,18 +64,22 @@ server <- function(input, output) {
   })
   
   # For page 3
+  
+  #create scatter plot output
   output$scatter_pg3 <- renderPlot({
-    
+    #create title using y user input
     title <- paste0("Scatter Plot: ", "CO2 per Capita", " v. ", input$y_var_pg3)
     
-    
+    #plot data using ggplot
     plot <- ggplot(data = final_data) +
-      geom_point(mapping = aes_string(x = final_data$co2.per.capita, y = input$y_var_pg3)) +
+      geom_point(mapping = aes_string(x = final_data$co2.per.capita, 
+                                      y = input$y_var_pg3)) +
       labs(x = "CO2 per Capita", y = input$y_var_pg3, title = title)
-    
+    #add statement to include trend box option for user
     if (input$smooth) {
       plot <- plot + geom_smooth(mapping = 
-                                   aes_string(x = final_data$co2.per.capita, y = input$y_var_pg3))
+                                   aes_string(x = final_data$co2.per.capita, 
+                                              y = input$y_var_pg3))
     }
     
     plot
@@ -84,15 +87,17 @@ server <- function(input, output) {
   })
   
   # For page 4
+  
+  #create scatter plot output
   output$scatter <- renderPlot({
-    
+    #create title using y user input
     title <- paste0("Scatter Plot: ", input$x_var, " v. ", input$y_var)
     
-    
+    #plot data using ggplot
     plot <- ggplot(data = final_data) +
       geom_point(mapping = aes_string(x = input$x_var, y = input$y_var)) +
       labs(x = input$x_var, y = input$y_var, title = title)
-    
+    #add statement to include trend box option for user
     if (input$smooth) {
       plot <- plot + geom_smooth(mapping = 
                                    aes_string(x = input$x_var, y = input$y_var))
